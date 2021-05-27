5936d589aaafdd10426c5026eb2569b537893dfa
Gurtej Singh <40589400+Gurtejsingh06@users.noreply.github.com>
2021-05-28 01:50:45 +0530

    Warm Up

---
+++ WarmUp/Sum_of_n_natural_numbers.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define mod 1e9+7
+//#define for(i,a,b) for(long long int i=a;i<b;i++)
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define endl "\n"
+using namespace std;
+
+typedef long long int ll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pi;
+
+
+void solve(){
+	ll n;
+	cin >> n;
+	ll sum = (n*(n+1))/2;
+	cout << sum << endl;
+}
+
+int main(){
+	
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	//ll t; cin >> t;
+	//while(t--) 
+	solve();
+
+	
+	return 0;
+
+}
